d057db5e5e0e286745835150f7491a643da0be88
rayofhopejp <ray.oshikawa@gmail.com>
2024-12-07 14:41:52 +0900

    count

--- ruby/app.rb
+++ ruby/app.rb
@@ -626,7 +626,7 @@ module Isuports
         logger.error("!!!!!existing_players_on_csv!!!!! #{"'"+existing_players_on_csv.join("','")+"'"}")
         player_count = tenant_db.execute("SELECT COUNT(*) as count FROM player WHERE id IN (#{"'"+existing_players_on_csv.join("','")+"'"})")
         logger.error("!!!!!player_count!!!!! #{player_count}")
-        if player_count != existing_players_on_csv.size
+        if player_count.count != existing_players_on_csv.size
           raise HttpError.new(400, "some player not found")
         end
         
